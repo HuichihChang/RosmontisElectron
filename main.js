e2e206e84203abdfdcc53d093628a801c14f930e
Huichih Chang <misaka10212@outlook.com>
2023-12-12 12:37:53 +0800

    初始化库

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require("electron");
+
+const electron = require("electron");
+
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+const Menu = electron.Menu;
+
+function createWindow() {
+  // 隐藏菜单栏
+  Menu.setApplicationMenu(null);
+  // Create the browser window.设置窗口宽高，最小宽高，图标等
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 1280,
+    minHeight: 800,
+    resizable: true,
+    allowRunningInsecureContent: true,
+    experimentalCanvasFeatures: true,
+    icon: "./favicon.ico",
+  });
+  mainWindow.loadFile("dist/rosmontis/index.html");
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
